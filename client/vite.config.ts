fd0c7099d7aeacfe46c99ca17688708ec785c3e4
Tushar <47114550+tusharmaharana@users.noreply.github.com>
2024-01-20 23:45:29 +0530

    added tailwind css with shadcn/ui

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgrPlugin from 'vite-plugin-svgr';
 import checker from 'vite-plugin-checker';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -26,6 +27,11 @@ export default defineConfig({
 			typescript: true,
 		}),
 	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 	server: {
 		port: 3000,
 	},
