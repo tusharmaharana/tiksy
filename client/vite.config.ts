80792f9d96db522ebe914834db1d899b7f269906
Tushar <47114550+tusharmaharana@users.noreply.github.com>
2024-01-22 03:41:47 +0530

    fix: output directory issue

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,16 +6,6 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	// This changes the output dir from dist to build
-	build: {
-		outDir: 'build',
-		minify: 'esbuild',
-		sourcemap: false,
-		chunkSizeWarningLimit: 1024,
-		rollupOptions: {
-			cache: false,
-		},
-	},
 	plugins: [
 		react(),
 		svgrPlugin({
